a8326c59ab1c9800e9af1b7dda0e320a96ec6033
Anderson Lopes <anderson.lopes.2010@gmail.com>
2019-11-22 22:21:28 +0000

    Commit inicial.

---
+++ src/main/java/com/lopes/PraticaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.lopes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraticaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraticaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lopes/PraticaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lopes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraticaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
